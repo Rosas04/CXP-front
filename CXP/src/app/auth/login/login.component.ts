82f2308542dcb68fca80f1f8914c1b9786ca7124
YeyoSM <152399977+YeyoSM@users.noreply.github.com>
2024-10-06 10:04:11 -0500

    from

---
+++ CXP/src/app/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  login() {
+    // Lógica para autenticarse con username y password
+    alert(`Inicio de sesión para ${this.username}`);
+  }
+}
